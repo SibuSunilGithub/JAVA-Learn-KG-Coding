fbc50a6c41938195f99c10dde27d4c349c7baf68
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-03-09 23:20:58 +0530

    Star Pattern Print
    Star Pattern Print Type 1

---
+++ Pr0003-StarPrintByPrintln.java
@@ -0,0 +1,12 @@
+// Print Star Pattern By Println Statements
+
+class StarPrintByPrintln{
+	public static void main(String []args)
+	{
+		System.out.println("*");
+		System.out.println("* *");
+		System.out.println("* * *");
+		System.out.println("* * * *");
+		System.out.println("* * * * *");
+	}
+}
