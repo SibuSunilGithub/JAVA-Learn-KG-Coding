c5902fa4d1d5a3a296d4cef3671787b5f7aeb4bf
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-03-11 23:39:29 +0530

    Variable Print Values

---
+++ Pr0006-VariablePrint.java
@@ -0,0 +1,10 @@
+// Variable Value Print
+
+class VariablePrint{
+	public static void main(String []args){
+		int v = 512;
+		System.out.println("1st Value: "+v);
+		v = 234;
+		System.out.println("2nd Value: "+v);
+	}
+}
